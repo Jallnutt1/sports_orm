013a53643059c70fb348865c167b162e7f93f5c2
Jason_A <{ID}+{username}@users.noreply.github.com>
2021-04-22 20:31:15 -0400

    setting up for Sport_ORM_2

--- leagues/views.py
+++ leagues/views.py
@@ -6,8 +6,26 @@ from . import team_maker
 def index(request):
 	context = {
 		"leagues": League.objects.all(),
+		"baseball_leagues": League.objects.filter(sport="Baseball"),
+		"womens_leagues": League.objects.filter(name__contains="Womens"),
+		"hockey_leagues": League.objects.filter(name__contains="Hockey"),
+		"no_football_leagues": League.objects.exclude(sport="Football"),
+		"conference_leagues": League.objects.filter(name__contains="Conference"),
+		"atlantic_leagues": League.objects.filter(name__startswith="Atlantic"),
+
 		"teams": Team.objects.all(),
+		"dallas_teams": Team.objects.filter(location="Dallas"),
+		"raptors_teams": Team.objects.filter(team_name__contains="Raptors"),
+		"city_teams": Team.objects.filter(location__contains="City"),
+		"t_teams": Team.objects.filter(team_name__startswith="T"),
+		"rev_team_order": Team.objects.order_by('-team_name'),
+
 		"players": Player.objects.all(),
+		"cooper_players":Player.objects.filter(last_name="Cooper"),
+		"joshua_players":Player.objects.filter(first_name="Joshua"),
+		"joshua_players":Player.objects.filter(first_name="Joshua"),
+		"no_joshua_coopers":Player.objects.filter(last_name="Cooper").exclude(first_name="Joshua"),
+		"alexander_and_wyatt":Player.objects.filter(first_name="Alexander" and "Wyatt"),
 	}
 	return render(request, "leagues/index.html", context)
 
